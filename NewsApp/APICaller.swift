ccd6b9f321d3b8dee12d21527eed8234985d0417
Pham Dinh Phuong Nam <namsgu123@gmail.com>
2022-07-21 21:50:49 +0700

    Initial Commit

---
+++ NewsApp/APICaller.swift
@@ -0,0 +1,65 @@
+//
+//  APICaller.swift
+//  NewsApp
+//
+//  Created by namghjk on 18/07/2022.
+//
+
+import Foundation
+
+
+final class APICaller{
+    static let shared = APICaller()
+    
+    struct Constains{
+        static let topHeadLinesURL = URL(string: "https://newsapi.org/v2/everything?q=tesla&from=2022-06-20&sortBy=publishedAt&apiKey=c79fa879b12441329c169a15bcee5c50")
+    }
+    
+    
+    private init(){}
+    
+    public func getTopStories(completion : @escaping (Result<[Article],Error>) -> Void){
+        guard let url = Constains.topHeadLinesURL else{
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data =  data{
+                
+                do{
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    
+                    print("Articles : \(result.articles.count)")
+                    
+                completion(.success(result.articles))
+                }
+                catch{
+                    print(error)
+                }
+                
+            }
+        }
+        task.resume()
+    }
+}
+
+//Models
+
+struct APIResponse: Codable{
+    let articles: [Article]
+}
+
+struct Article: Codable{
+    let source: Source
+    let title:String
+    let description:String?
+    let url:String?
+    let urlToImage:String?
+    let publishedAt:String
+}
+
+struct Source: Codable{
+    let name: String
+}
---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by namghjk on 18/07/2022.
+//
+
+import UIKit
+import SafariServices
+
+
+//TableView
+//Custom Cell
+//API Caller
+//Open news story
+//search for news stories
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(NewsTableViewCell.self, forCellReuseIdentifier: NewsTableViewCell.identifier)
+        return table
+    }()
+    
+    private var articles = [Article]()
+    private var viewModels = [NewsTableViewCellViewModel]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title="news"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.backgroundColor = .systemBackground
+        
+        APICaller.shared.getTopStories {[weak self]result in
+            switch result{
+            case.success(let articles):
+                self?.articles = articles
+                self?.viewModels = articles.compactMap({
+                NewsTableViewCellViewModel(
+                    title: $0.title,
+                    subtitle: $0.description ?? "No description " ,
+                    imageURL: URL(string: $0.urlToImage ?? "")
+                )
+            })
+                
+                DispatchQueue.main.sync {
+                    self?.tableView.reloadData()
+                }
+                
+            case.failure(let error):
+                print(error)
+            }
+        }
+    }
+
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+
+    //Table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       guard let cell = tableView.dequeueReusableCell(withIdentifier: NewsTableViewCell.identifier, for: indexPath)
+        as? NewsTableViewCell else{
+            fatalError()
+        }
+        cell.configure(with: viewModels[indexPath .row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated:  true)
+        let artilce = articles[indexPath.row]
+        
+        guard let url = URL(string: artilce.url ?? "")else{
+            return
+        }
+        
+        let vc = SFSafariViewController(url: url)
+        present(vc, animated: true)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+}
+
